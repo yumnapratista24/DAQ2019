30017b98d9af3549439189cfcf57892eecd90ae5
Yumna Pratista Tastaftian <yumnanaruto@gmail.com>
2019-06-05 21:08:48 +0700

    UI yumna - Alarm settings

---
+++ app/src/main/java/com/android/test/ueueueu/Settings.java
@@ -0,0 +1,25 @@
+package com.android.test.ueueueu;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * Created by 642174 on 30/05/2019.
+ */
+
+public class Settings extends PreferenceFragmentCompat{
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey){
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
