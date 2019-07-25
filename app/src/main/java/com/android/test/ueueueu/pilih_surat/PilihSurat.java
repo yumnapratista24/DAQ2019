b6d30918890c9d21c925c8e841748877cc783624
Yumna Pratista <yumnanaruto@gmail.com>
2019-07-26 01:36:41 +0700

    berhasil yg awal2 hehe

--- app/src/main/java/com/android/test/ueueueu/pilih_surat/PilihSurat.java
+++ app/src/main/java/com/android/test/ueueueu/pilih_surat/PilihSurat.java
@@ -1,14 +1,19 @@
 package com.android.test.ueueueu.pilih_surat;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -27,21 +32,77 @@ public class PilihSurat extends Activity {
     private ListView lv;
     private DatabaseHelper dbHelper;
 
+    public PilihSurat(){
+
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pilih_surat);
+        View view = getLayoutInflater().inflate(R.layout.activity_pilih_surat, null);
+        renderSurah(view);
+    }
 
-        dbHelper = new DatabaseHelper(this);
+    public void showFirstTimer(Activity activity){
+        //then we will inflate the custom alert dialog xml that we created
+        final View dialogView = activity.getLayoutInflater().inflate(R.layout.layout_onboarding, null);
+
+        renderSurah(dialogView);
+
+        //Now we need an AlertDialog.Builder object
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+
+        //setting the view of the builder to our custom view that we already inflated
+        builder.setView(dialogView);
+
+        //finally creating the alert dialog and displaying it
+        final AlertDialog alertDialog = builder.create();
+
+        // Dialog if user didnt choose any surah
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity);
+
+        alertDialogBuilder.setTitle("Anda harus memilih minimal 1 surat!");
+        alertDialogBuilder.setPositiveButton("Okay",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+        Button saveButton = (Button) dialogView.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkSurahChoosed(view)){
+                    alertDialog.dismiss();
+                    Toast.makeText(view.getContext(), "Surah saved!", Toast.LENGTH_SHORT).show();
+                } else{
+                    alertDialogBuilder.show();
+                }
+            }
+        });
+        alertDialog.show();
+    }
+
+    private void renderSurah(View view){
+        dbHelper = new DatabaseHelper(view.getContext());
 
         List<Surah> surahs = dbHelper.getAllSurah();
         Log.i("CEK CEK",surahs.toString());
 
-        lv = (ListView) findViewById(R.id.list);
+        lv = (ListView) view.findViewById(R.id.list);
 
-        surahGetter = new SurahGetter(this, surahs, dbHelper);
+        surahGetter = new SurahGetter(view.getContext(), surahs, dbHelper);
 
         lv.setAdapter(surahGetter);
     }
 
+    private Boolean checkSurahChoosed(View view){
+        dbHelper = new DatabaseHelper(view.getContext());
+
+        if(dbHelper.getAllChoosenSurah().size() < 1) return false;
+        return true;
+    }
 }
